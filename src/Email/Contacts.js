515127c7050af4853b90da3de193296951690386
cooljasonmelton <cooljasonmelton>
2020-07-11 01:54:57 -0500

    changed email items from flex to semantic grid

--- src/Email/Contacts.js
+++ src/Email/Contacts.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 
 //styling
 import './Contact.css';
+import './Email.css';
+
 import { Input, Segment, List, ListItem } from 'semantic-ui-react'
 
 //components
@@ -16,27 +18,6 @@ const Contacts = () => {
       <Input placeholder="Search..."
         onChange={e => setSearchItem(e.target.value)}
         value={searchItem}/>
-
-      <List divided>
-        <List.Item>
-          <List horizontal divided>
-            <List.Item> name </List.Item>
-            <List.Item> email </List.Item>
-            <List.Item> tags </List.Item>
-          </List>
-        </List.Item>
-        <List.Item>
-          <List horizontal divided className="contact-entry">
-            <List.Item className="contact-entry"> Jason Melton </List.Item>
-            <List.Item className="contact-entry"> jason@email.com </List.Item>
-            <List.Item className="contact-entry"> friend, enemy, cat-liker </List.Item>
-          </List>
-        </List.Item>
-      </List>
-
-      
-
-
     </Segment>
   );
 }
--- src/Email/EmailContainer.js
+++ src/Email/EmailContainer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 //styling
 import './Email.css';
-import { Segment } from 'semantic-ui-react'
+import { Segment, Grid } from 'semantic-ui-react'
 
 //components
 import TemplateMenu from './TemplateMenu';
@@ -12,10 +12,20 @@ import Contacts from './Contacts';
 const EmailContainer = () => {
   
   return (
-    <Segment className="EmailContainer">
-      <TemplateMenu/>
-      <Template/>
-      <Contacts/>
+    <Segment className="EmailContainer" style={{height: '80%'}}>
+      <Grid style={{height: '100%'}} columns={3}>
+        <Grid.Row stretched>
+          <Grid.Column>
+            <TemplateMenu/> 
+          </Grid.Column>
+          <Grid.Column>
+            <Template/>
+          </Grid.Column>
+          <Grid.Column>
+            <Contacts/>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </Segment>
   );
 }

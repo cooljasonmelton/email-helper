fb9526b29de7a3172f02380fbda212acdae3a618
cooljasonmelton <cooljasonmelton>
2020-07-11 00:54:52 -0500

    installed react redux for provider

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 // redux
+import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import reducer from './reducers/index'
+
 
 // styling
 import './index.css';
@@ -17,7 +19,9 @@ const store = createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );

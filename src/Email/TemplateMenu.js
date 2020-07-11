bffd62af58de5eb6290cf9211ba8ac8de9f87774
cooljasonmelton <cooljasonmelton>
2020-07-11 00:03:17 -0500

    set up templatemenu design

--- src/Email/TemplateMenu.js
+++ src/Email/TemplateMenu.js
@@ -1,21 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 //styling
 import './Email.css';
-import { Segment } from 'semantic-ui-react'
-
-
-//components
-
+import { Segment, Button, Input } from 'semantic-ui-react'
 
 const TemplateMenu = () => {
-  
-  return (
-    <Segment className="TemplateMenu">
+    const [searchItem, setSearchItem] = useState('')
 
+    return (
+        <Segment className="TemplateMenu">
+            <div className="template-menu-options">
+                <Button >+</Button>
+                <Input placeholder='Search...'
+                    onChange={e => setSearchItem(e.target.value)}
+                    value={searchItem}/>
+            </div>
 
-    </Segment>
-  );
+            {/* SAMPLE OF WHAT TEMPLATES WILL LOOK LIKE */}
+            <Segment> 
+                <Segment className="template-item" onClick={null}>
+                    <h3>name of template</h3>
+                    <p>sample text sample text sample text...</p>               
+                    <Button size="mini" >x</Button>
+                </Segment>
+            </Segment>
+        </Segment>
+    );
 }
 
 export default TemplateMenu;

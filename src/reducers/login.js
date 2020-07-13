d04b1fc53995a9588c78d796474306a7f0f3084c
cooljasonmelton <cooljasonmelton>
2020-07-13 05:05:00 -0500

    adjusted login to have sign up

--- src/reducers/login.js
+++ src/reducers/login.js
@@ -1,6 +1,7 @@
 export default function login(state = { user: ""}, action) {
     switch (action.type) {
         case 'LOGIN_USER':
+            console.log(action.payload)
             return action.payload
         default:
             return state

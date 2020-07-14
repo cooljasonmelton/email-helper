21254e7ef71d9632917a93da838fefb880f5b784
cooljasonmelton <cooljasonmelton>
2020-07-14 01:26:48 -0500

    user can create new templates

--- src/Email/NewTemplate.js
+++ src/Email/NewTemplate.js
@@ -1,36 +1,74 @@
 import React, {useState} from 'react';
 
+//redux
+import { connect } from 'react-redux'
+
 //styling
 import './Email.css';
-import { Divider, Button, TextArea, Input, Form } from 'semantic-ui-react'
+import { Divider, TextArea, Input, Form } from 'semantic-ui-react'
 
-const NewTemplate = () => {
+const NewTemplate = props => {
     const [name, setName] = useState('')
     const [subject, setSubject] = useState('')
     const [body, setBody] = useState('')
 
+    const saveNewTemplate = () => {
+        console.log(props.state)
+        if (!props.state.login.id) return
+
+        const reqObj = {
+            method: "POST",
+            headers: {
+              'Content-Type': 'application/json' 
+            },
+            body: JSON.stringify({
+                userId: props.state.login.id,
+                name, 
+                subject, 
+                body 
+            })
+        }
+        
+        fetch('http://localhost:3000/templates', reqObj)
+        .then(r=>r.json())
+        .then(data=>console.log(data));
+    }
+
     return (
         <>
-        <Input placeholder="template name" 
-            value={name} 
-            onChange={e => setName(e.target.value)}/>
+            <Input placeholder="template name" 
+                value={name}
+                onBlur={saveNewTemplate} 
+                onChange={e => setName(e.target.value)}/>
 
-        <Divider/>
+            <Divider/>
 
-        <Input placeholder="subject"
-            onChange={e => setSubject(e.target.value)}
-            value={subject}/>
-            
-        <Form className="template-text" >
-            <TextArea className="template-text" 
-                placeholder="body"
-                onChange={e => setBody(e.target.value)}
-                value={body}/>
-        </Form>
-        
-        <Button>Save</Button>
+            <Input placeholder="subject"
+                onBlur={saveNewTemplate} 
+                onChange={e => setSubject(e.target.value)}
+                value={subject}/>
+                
+            <Form className="template-text" >
+                <TextArea className="template-text" 
+                    onBlur={saveNewTemplate} 
+                    placeholder="body"
+                    onChange={e => setBody(e.target.value)}
+                    value={body}/>
+            </Form>
         </>
     );
 }
 
-export default NewTemplate;
+const mapDispatchToProps = dispatch => {
+    return {
+      login: formData => dispatch({ type: 'LOGIN_USER', payload: formData })
+    };
+};
+  
+const mapStateToProps = state => {
+    return {
+        state
+    }
+}
+  
+export default connect(mapStateToProps, mapDispatchToProps)(NewTemplate);
--- src/reducers/login.js
+++ src/reducers/login.js
@@ -1,7 +1,6 @@
 export default function login(state = { user: ""}, action) {
     switch (action.type) {
         case 'LOGIN_USER':
-            console.log(action.payload)
             return action.payload
         default:
             return state
